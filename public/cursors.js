d75681a0119a7f4995e2934ca38c1aed17305fb4
Ted Summer <ted.summer2@gmail.com>
2025-07-17 21:33:29 -0400

    cursors

--- public/cursors.js
+++ public/cursors.js
@@ -3,7 +3,14 @@ const cursors = [
   "url(cursor-paint-brush.png) 15 15, auto",
   "url(cursor-pencil.png) 5 12, auto",
   "url(tool-ptr-bucket-fill.png) 18 20, auto",
+  "url(cursor-lobster.cur), auto",
+  "url(cursor-scimi.cur), auto",
 ];
-let randomCursor = Math.floor(Math.random() * cursors.length);
-const body = document.querySelector("body");
-body.style.cursor = cursors[randomCursor];
+
+let currentCursorIndex = Math.floor(Math.random() * cursors.length);
+
+document.getElementById("logo").addEventListener("click", function () {
+  console.log("clicked");
+  currentCursorIndex = (currentCursorIndex + 1) % cursors.length;
+  document.body.style.cursor = cursors[currentCursorIndex];
+});
